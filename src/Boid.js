a6319858882a059d271a398cfa4becfc74f29f7d
Shane Phelan <shane.phelan@itv.com>
2014-01-30 22:57:30 +0000

    Initial commit of folder structure

---
+++ src/Boid.js
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: shanephelan
+ * Date: 30/01/2014
+ * Time: 20:10
+ * To change this template use File | Settings | File Templates.
+ */
+(function() {
+
+    'use strict';
+
+    var Boid = function() {
+
+    };
+
+    Boid.prototype = {
+
+    };
+})
---
+++ src/Math.js
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: shanephelan
+ * Date: 30/01/2014
+ * Time: 21:13
+ * To change this template use File | Settings | File Templates.
+ */
+(function() {
+
+})
